8f55398207d63b5b45a622fd3327ce7a19e3003f
mahia <mahiarahman2003@gmail.com>
2026-02-06 13:36:03 +0600

    profile image

--- index.js
+++ index.js
@@ -1,77 +1,96 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const port = process.env.PORT || 5000
-const app = express()
+const port = process.env.PORT || 5000;
+const app = express();
+
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: true,
     credentials: true,
   })
-)
+);
 
-app.use(express.json())
+app.use(express.json());
 
-const client = new MongoClient(process.env.MONGODB_URI, {
+
+const uri = process.env.DB_URI;
+
+
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 
 async function run() {
   try {
-    await client.connect()
+    await client.connect();
 
-    const db = client.db('localchef')
-    const mealsCollection = db.collection('meals')
-    const reviewsCollection = db.collection('reviews')
-    const usersCollection = db.collection('users')
+    const db = client.db("localchef");
 
+    const usersCollection = db.collection("users");
+    const mealsCollection = db.collection("meals");
+    const reviewsCollection = db.collection("reviews");
 
-    app.post('/users', async (req, res) => {
-      const user = req.body
+    console.log("MongoDB Connected Successfully");
 
-      const existingUser = await usersCollection.findOne({
-        email: user.email,
-      })
+  
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
 
-      if (existingUser) {
-        return res.send({ message: 'User already exists' })
+        const existingUser = await usersCollection.findOne({
+          email: user.email,
+        });
+
+        if (existingUser) {
+          return res.send({ message: "User already exists" });
+        }
+
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
       }
+    });
 
-      const result = await usersCollection.insertOne(user)
-      res.send(result)
-    })
-
-  
-    app.get('/meals-limit-6', async (req, res) => {
-      const meals = await mealsCollection.find().limit(6).toArray()
-      res.send(meals)
-    })
+    
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
   
-    app.get('/reviews', async (req, res) => {
-      const reviews = await reviewsCollection.find().toArray()
-      res.send(reviews)
-    })
+    app.get("/meals-limit-6", async (req, res) => {
+      const meals = await mealsCollection.find().limit(6).toArray();
+      res.send(meals);
+    });
 
-    app.get('/', (req, res) => {
-      res.send('Server running')
-    })
+    
+    app.get("/reviews", async (req, res) => {
+      const reviews = await reviewsCollection.find().toArray();
+      res.send(reviews);
+    });
 
-    console.log('MongoDB Connected')
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
-run()
+run();
+
+
+app.get("/", (req, res) => {
+  res.send("LocalChefBazar Server Running");
+});
+
 
 app.listen(port, () => {
-  console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
